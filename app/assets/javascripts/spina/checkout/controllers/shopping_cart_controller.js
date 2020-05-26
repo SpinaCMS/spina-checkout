f382d4792702cc74add81655b0e413e831505b92
Bram Jetten <mail@bramjetten.nl>
2020-05-26 13:23:52 +0200

    Shopping cart aanpassen

---
+++ app/assets/javascripts/spina/checkout/controllers/shopping_cart_controller.js
@@ -0,0 +1,59 @@
+(() => {
+  const application = Stimulus.Application.start()
+
+  application.register("shopping-cart", class extends Stimulus.Controller {
+    static get targets() {
+      return []
+    }
+
+    connect() {
+
+    }
+
+    setQuantity(order_item_url, quantity) {
+      fetch(order_item_url, {
+        method: 'PUT',
+        headers: {
+          'X-CSRF-Token': Rails.csrfToken(),
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          quantity: quantity
+        })
+      }).then(function(response) {
+          if (response.status == 200) this.sidebar.fetchSummary()
+        }.bind(this))
+    }
+
+    add(event) {
+      let field = event.currentTarget.parentElement.querySelector("input")
+      let quantity = parseInt(field.value)
+      field.value = Math.min(quantity + 1, this.max(field))
+      field.dispatchEvent(new Event('change'))
+    }
+
+    subtract(event) {
+      let field = event.currentTarget.parentElement.querySelector('input')
+      let quantity = parseInt(field.value)
+      field.value = Math.max(quantity - 1, 1)
+      field.dispatchEvent(new Event('change'))
+    }
+
+    addToCart(event) {
+      let field = event.currentTarget
+      field.value = Math.min(field.value, this.max(field))
+      field.value = Math.max(field.value, 1)
+      this.setQuantity(field.dataset.orderItemUrl, field.value)
+    }
+
+    max(field) {
+      return parseInt(field.dataset.maximum)
+    }
+
+    get sidebar() {
+      return document.querySelector('.sidebar-summary').controller
+    }
+
+  })
+})()
--- app/controllers/spina/checkout/order_items_controller.rb
+++ app/controllers/spina/checkout/order_items_controller.rb
@@ -1,12 +1,25 @@
 module Spina
   module Checkout
     class OrderItemsController < CheckoutController
+      before_action :current_order_building?
 
       def update
+        @order_item = current_order.order_items.find(params[:id])
+        @order_item.update(quantity: params[:quantity])
+        head :ok
       end
 
       def destroy
+        @order_item = current_order.order_items.find(params[:id])
+        @order_item.destroy
+        redirect_to wizard_path(:shopping_cart)
       end
+
+      private
+
+        def current_order_building?
+          raise unless current_order.building?
+        end
 
     end
   end
