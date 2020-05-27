9b2ef2d17661525389f76d8c7c5a3d17258af0c0
Bram Jetten <mail@bramjetten.nl>
2020-05-27 16:51:50 +0200

    Railtie

--- lib/spina/checkout/railtie.rb
+++ lib/spina/checkout/railtie.rb
@@ -2,7 +2,7 @@ module Spina
   module Shop
     class Railtie < Rails::Railtie
 
-      initializer "spina_shop.assets.precompile" do |app|
+      initializer "spina_checkout.assets.precompile" do |app|
         app.config.assets.precompile += %w(spina_checkout_manifest.js)
       end
 
