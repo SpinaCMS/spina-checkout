102469057a8e2cd7e08e20f1574eb08a01afba5d
Bram Jetten <mail@bramjetten.nl>
2021-08-24 15:08:49 +0200

    Analytics

---
+++ app/assets/javascripts/spina/checkout/controllers/analytics_controller.js
@@ -0,0 +1,22 @@
+(() => {
+  const application = Stimulus.Application.start()
+
+  application.register("analytics", class extends Stimulus.Controller {
+    
+    connect() {
+      this.element[this.identifier] = this
+      trackCheckout()
+    }
+    
+    trackCheckout() {
+      ga("ec:setAction", "checkout", {
+        "step": this.step
+      })
+    }
+    
+    get step() {
+      return parseInt(this.element.dataset.checkoutStep)
+    }
+    
+  })
+})()
