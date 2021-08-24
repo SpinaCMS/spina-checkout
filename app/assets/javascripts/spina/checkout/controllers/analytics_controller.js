bd8daf0156539114627a36df08550ae3fbc267b6
Bram Jetten <mail@bramjetten.nl>
2021-08-24 15:34:50 +0200

    Add product

--- app/assets/javascripts/spina/checkout/controllers/analytics_controller.js
+++ app/assets/javascripts/spina/checkout/controllers/analytics_controller.js
@@ -9,6 +9,14 @@
     }
     
     trackCheckout() {
+      ga("ec:addProduct", {
+        'id': 1,
+        'name': "Checkout",
+        'category': "Spina",
+        'brand': "denkGroot",
+        'price': 0,
+        'quantity': 1
+      })
       ga("ec:setAction", "checkout", {
         "step": this.step
       })
